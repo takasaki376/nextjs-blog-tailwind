511f431952630536e31926f6d31e27837818dff1
takasaki376 <takasaki.a201070@outlook.jp>
2021-08-10 17:48:52 +0900

    💄 update

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+import { HomeLayout } from "src/pages/index/HomeLayout";
+
+const Home: NextPage = () => {
+  return <HomeLayout title="top">blog</HomeLayout>;
+};
+export default Home;
---
+++ src/pages/login.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from "next";
+
+const Login: NextPage = () => {
+  return <div>google</div>;
+};
+export default Login;
