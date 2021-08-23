28828baf7a827674629b945dc820d7b73e17aeb5
takasaki376 <takasaki.a201070@outlook.jp>
2021-08-23 18:05:20 +0900

    🔨 firebase-auth

---
+++ src/context/user/withUser.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from "next";
+import {
+  AuthAction,
+  // useAuthUser,
+  withAuthUser,
+} from "next-firebase-auth";
+// import { useCallback } from "react";
+
+// const useFetcher = () => {
+//   const authUser = useAuthUser();
+//   const fetcher = useCallback(
+//     async (url: string) => {
+//       const idToken = await authUser.getIdToken();
+//       const res = await fetch(url, {
+//         headers: { authorization: `Bearer ${idToken}` },
+//       });
+//       if (res.status !== 200 && res.status !== 201) {
+//         throw new Error();
+//       }
+//       const json = await res.json();
+//       return json;
+//     },
+//     [authUser]
+//   );
+//   return fetcher;
+// };
+
+/**
+ * @package
+ */
+export const withUser = (Component: NextPage<any>) => {
+  return withAuthUser({
+    whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
+    whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+  })((props) => {
+    // useUserFetch();
+    // const fetcher = useFetcher();
+    return (
+      // <SWRConfig value={{ fetcher }}>
+      <Component {...props} />
+      // </SWRConfig>
+    );
+  });
+};
